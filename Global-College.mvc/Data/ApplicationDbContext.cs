f39e1b8fa6a1dd284dcdd17079e6def7ac4a0f51
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-03-31 18:40:49 +0100

    Creating DbContext. Update appsettings.Development Update Program

--- Global-College.mvc/Data/ApplicationDbContext.cs
+++ Global-College.mvc/Data/ApplicationDbContext.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Global_College.domain.Models.Administrator;
+using Global_College.domain.Models.Faculty;
+using Global_College.domain.Models.Student;
 
 namespace Global_College.mvc.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+            : base(options)     // This line comes with the project
         {
         }
+
+        // Each DbSet represents one table on the database
+        // Administrator
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseEnrolment> CourseEnrolments { get; set; }
+        public DbSet<FacultyCourseAssignment> FacultyCourseAssignments { get; set; }
+        public DbSet<FacultyProfile> FacultyProfiles { get; set; }
+        public DbSet<StudentProfile> StudentProfiles { get; set; }
+
+        // Faculty
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<AssignmentResult> AssignmentResults { get; set; }
+        public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+
+        // Student
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<ExamResult> ExamResults { get; set; }
     }
 }
