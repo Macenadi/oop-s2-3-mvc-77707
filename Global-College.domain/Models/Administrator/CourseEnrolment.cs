12de15b465a15db4567452fa7b4311490ecc3b73
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-03-31 17:56:22 +0100

    Last for models

--- Global-College.domain/Models/Administrator/CourseEnrolment.cs
+++ Global-College.domain/Models/Administrator/CourseEnrolment.cs
@@ -10,7 +10,7 @@ namespace Global_College.domain.Models.Administrator
     {
         public int Id { get; set; }
         public DateOnly EnrolDate { get; set; }
-        public string Status { get; set; } = String.Empty; // Status of the enrolment (e.g., "Enrolled", "Completed", "Dropped").
+        public string Status { get; set; } = string.Empty; // Status of the enrolment (e.g., "Enrolled", "Completed", "Dropped").
         public Course? Course { get; set; }     // Navigation property to Course. Allows access to the related Course entity.
         public int CourseId { get; set; }       // Foreign key to Course. Calls the ID from the Course class
         public StudentProfile? StudentProfile { get; set; }   // Navigation property to StudentProfile. Allows access to the related Student entity.
--- Global-College.domain/Models/Student/ExamResult.cs
+++ Global-College.domain/Models/Student/ExamResult.cs
@@ -14,8 +14,8 @@ namespace Global_College.domain.Models.Student
         public string Grade { get; set; } = string.Empty; // Grade can be represented as a string (e.g., A, B, C) or as a letter grade.
         public Exam? Exam { get; set; }         // Navigation property to Exam. Allows access to the related Exam entity.
         public int ExamId { get; set; }         // Foreign key to Exam. Calls the ID from the Exam class.
-        public StudentProfile? StudentProfile { get; set; }   // Navigation property to Student. Allows access to the related Student entity.
-        public int StudentProfileId { get; set; }      // Foreign key to Student. Calls the ID from the Student class.
+        public StudentProfile? StudentProfile { get; set; }   // Navigation property to StudentProfile. Allows access to the related Student entity.
+        public int StudentProfileId { get; set; }      // Foreign key to StudentProfile. Calls the ID from the StudentProfile class.
 
 
         public void Validate() 
