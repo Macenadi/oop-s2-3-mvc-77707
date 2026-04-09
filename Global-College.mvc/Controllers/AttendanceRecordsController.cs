b3097ae26c1c32e849ab4868c107434f7979bcd0
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-04-09 03:50:23 +0100

    commit

--- Global-College.mvc/Controllers/AttendanceRecordsController.cs
+++ Global-College.mvc/Controllers/AttendanceRecordsController.cs
@@ -1,10 +1,12 @@
 ﻿using Global_College.domain.Models.Faculty;
 using Global_College.mvc.Data;
+using Global_College.mvc.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,19 +23,127 @@ namespace Global_College.mvc.Controllers
         }
 
         // GET: AttendanceRecords
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? branchId, int? courseId, DateOnly? date)
         {
-            var applicationDbContext = _context.AttendanceRecords
-                .Include(a => a.CourseEnrolment)
-                    .ThenInclude(ce => ce.StudentProfile)
-                .Include(a => a.CourseEnrolment)
-                    .ThenInclude(ce => ce.BranchCourse)
-                        .ThenInclude(bc => bc.Branch)
-                .Include(a => a.CourseEnrolment)
-                    .ThenInclude(ce => ce.BranchCourse)
-                        .ThenInclude(bc => bc.Course);
+            var model = new AttendanceIndexViewModel
+            {
+                BranchId = branchId,
+                CourseId = courseId,
+                Date = date ?? DateOnly.FromDateTime(DateTime.Today)
+            };
 
-            return View(await applicationDbContext.ToListAsync());
+            await LoadFilters(model);
+
+            if (branchId.HasValue && courseId.HasValue)
+            {
+                var enrolments = await _context.CourseEnrolments
+                    .Include(ce => ce.StudentProfile)
+                    .Include(ce => ce.BranchCourse)
+                        .ThenInclude(bc => bc.Branch)
+                    .Include(ce => ce.BranchCourse)
+                        .ThenInclude(bc => bc.Course)
+                    .Where(ce =>
+                        ce.BranchCourse.BranchId == branchId.Value &&
+                        ce.BranchCourse.CourseId == courseId.Value &&
+                        ce.Status == "Enrolled")
+                    .OrderBy(ce => ce.StudentProfile.FullName)
+                    .ToListAsync();
+
+                var selectedDate = model.Date;
+
+                var attendanceRecords = await _context.AttendanceRecords
+                    .Where(a => a.Date == selectedDate)
+                    .ToListAsync();
+
+                model.Students = enrolments.Select(ce =>
+                {
+                    var existingAttendance = attendanceRecords
+                        .FirstOrDefault(a => a.CourseEnrolmentId == ce.Id);
+
+                    return new AttendanceStudentItemViewModel
+                    {
+                        CourseEnrolmentId = ce.Id,
+                        StudentProfileId = ce.StudentProfileId,
+                        StudentNumber = ce.StudentProfile.StudentNumber,
+                        FullName = ce.StudentProfile.FullName,
+                        Present = existingAttendance?.Present ?? false,
+                        AttendanceRecordId = existingAttendance?.Id
+                    };
+                }).ToList();
+            }
+
+            return View(model);
+        }
+
+        // POST: AttendanceRecords/SaveAttendance
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveAttendance(AttendanceIndexViewModel model)
+        {
+            if (!model.BranchId.HasValue)
+            {
+                ModelState.AddModelError("BranchId", "Please select a branch.");
+            }
+
+            if (!model.CourseId.HasValue)
+            {
+                ModelState.AddModelError("CourseId", "Please select a course.");
+            }
+
+            if (model.Students == null || !model.Students.Any())
+            {
+                ModelState.AddModelError("", "No students found for the selected branch and course.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadFilters(model);
+                return View("Index", model);
+            }
+
+            var selectedDate = model.Date;
+
+            var courseEnrolmentIds = model.Students
+                .Select(s => s.CourseEnrolmentId)
+                .ToList();
+
+            var existingAttendanceRecords = await _context.AttendanceRecords
+                .Where(a => courseEnrolmentIds.Contains(a.CourseEnrolmentId) && a.Date == selectedDate)
+                .ToListAsync();
+
+            foreach (var student in model.Students)
+            {
+                var existingRecord = existingAttendanceRecords
+                    .FirstOrDefault(a => a.CourseEnrolmentId == student.CourseEnrolmentId);
+
+                if (existingRecord == null)
+                {
+                    var newRecord = new AttendanceRecord
+                    {
+                        CourseEnrolmentId = student.CourseEnrolmentId,
+                        Date = selectedDate,
+                        Present = student.Present
+                    };
+
+                    _context.AttendanceRecords.Add(newRecord);
+                }
+                else
+                {
+                    existingRecord.Present = student.Present;
+                    _context.AttendanceRecords.Update(existingRecord);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Attendance saved successfully.";
+
+            return RedirectToAction(nameof(Index), new
+            {
+                branchId = model.BranchId,
+                courseId = model.CourseId,
+                date = model.Date.ToString("yyyy-MM-dd")
+            });
         }
 
         // GET: AttendanceRecords/Details/5
@@ -63,147 +173,32 @@ namespace Global_College.mvc.Controllers
             return View(attendanceRecord);
         }
 
-        // GET: AttendanceRecords/Create
-        public IActionResult Create()
+        private async Task LoadFilters(AttendanceIndexViewModel model)
         {
-            LoadDropDowns();
-            return View();
-        }
-
-        // POST: AttendanceRecords/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Date,Present,CourseEnrolmentId")] AttendanceRecord attendanceRecord)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(attendanceRecord);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            LoadDropDowns(attendanceRecord.CourseEnrolmentId);
-            return View(attendanceRecord);
-        }
-
-        // GET: AttendanceRecords/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var attendanceRecord = await _context.AttendanceRecords.FindAsync(id);
-            if (attendanceRecord == null)
-            {
-                return NotFound();
-            }
-
-            LoadDropDowns(attendanceRecord.CourseEnrolmentId);
-            return View(attendanceRecord);
-        }
-
-        // POST: AttendanceRecords/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Present,CourseEnrolmentId")] AttendanceRecord attendanceRecord)
-        {
-            if (id != attendanceRecord.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+            model.Branches = await _context.Branches
+                .OrderBy(b => b.Name)
+                .Select(b => new SelectListItem
                 {
-                    _context.Update(attendanceRecord);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AttendanceRecordExists(attendanceRecord.Id))
-                    {
-                        return NotFound();
-                    }
-
-                    throw;
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            LoadDropDowns(attendanceRecord.CourseEnrolmentId);
-            return View(attendanceRecord);
-        }
-
-        // GET: AttendanceRecords/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var attendanceRecord = await _context.AttendanceRecords
-                .Include(a => a.CourseEnrolment)
-                    .ThenInclude(ce => ce.StudentProfile)
-                .Include(a => a.CourseEnrolment)
-                    .ThenInclude(ce => ce.BranchCourse)
-                        .ThenInclude(bc => bc.Branch)
-                .Include(a => a.CourseEnrolment)
-                    .ThenInclude(ce => ce.BranchCourse)
-                        .ThenInclude(bc => bc.Course)
-                .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (attendanceRecord == null)
-            {
-                return NotFound();
-            }
-
-            return View(attendanceRecord);
-        }
-
-        // POST: AttendanceRecords/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var attendanceRecord = await _context.AttendanceRecords.FindAsync(id);
-
-            if (attendanceRecord != null)
-            {
-                _context.AttendanceRecords.Remove(attendanceRecord);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool AttendanceRecordExists(int id)
-        {
-            return _context.AttendanceRecords.Any(e => e.Id == id);
-        }
-
-        private void LoadDropDowns(int? selectedCourseEnrolmentId = null)
-        {
-            var enrolments = _context.CourseEnrolments
-                .Include(ce => ce.StudentProfile)
-                .Include(ce => ce.BranchCourse)
-                    .ThenInclude(bc => bc.Branch)
-                .Include(ce => ce.BranchCourse)
-                    .ThenInclude(bc => bc.Course)
-                .Select(ce => new
-                {
-                    ce.Id,
-                    Display = ce.StudentProfile.FullName + " - " +
-                              ce.BranchCourse.Branch.Name + " - " +
-                              ce.BranchCourse.Course.Name
+                    Value = b.Id.ToString(),
+                    Text = b.Name
                 })
-                .ToList();
+                .ToListAsync();
 
-            ViewData["CourseEnrolmentId"] = new SelectList(enrolments, "Id", "Display", selectedCourseEnrolmentId);
+            model.Courses = new List<SelectListItem>();
+
+            if (model.BranchId.HasValue)
+            {
+                model.Courses = await _context.BranchCourses
+                    .Where(bc => bc.BranchId == model.BranchId.Value)
+                    .Include(bc => bc.Course)
+                    .OrderBy(bc => bc.Course.Name)
+                    .Select(bc => new SelectListItem
+                    {
+                        Value = bc.CourseId.ToString(),
+                        Text = bc.Course.Name
+                    })
+                    .ToListAsync();
+            }
         }
     }
 }
---
+++ Global-College.mvc/Models/ViewModel/AttendanceIndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+namespace Global_College.mvc.ViewModels
+{
+    public class AttendanceIndexViewModel
+    {
+        public int? BranchId { get; set; }
+        public int? CourseId { get; set; }
+        public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+        public List<SelectListItem> Branches { get; set; } = new();
+        public List<SelectListItem> Courses { get; set; } = new();
+        public List<AttendanceStudentItemViewModel> Students { get; set; } = new();
+    }
+
+    public class AttendanceStudentItemViewModel
+    {
+        public int CourseEnrolmentId { get; set; }
+        public int StudentProfileId { get; set; }
+        public string StudentNumber { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public bool Present { get; set; }
+        public int? AttendanceRecordId { get; set; }
+    }
+}
