025254a01329bb2834f318b9f381c5aba6c8cf0a
Nádia Cardoso de Macedo <nadia_cardoso_macedo@hotmail.com>
2026-04-09 17:31:13 +0100

    last updates

---
+++ Global-College.mvc/Migrations/20260409161213_UpdateCourseRemoveDatesAddCreatedAt.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Global_College.mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCourseRemoveDatesAddCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "EndDate",
+                table: "Courses",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "StartDate",
+                table: "Courses",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateOnly(2025, 12, 31), new DateOnly(2025, 1, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateOnly(2025, 12, 31), new DateOnly(2025, 1, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateOnly(2025, 12, 31), new DateOnly(2025, 1, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateOnly(2025, 12, 31), new DateOnly(2025, 1, 1) });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateOnly(2025, 12, 31), new DateOnly(2025, 1, 1) });
+        }
+    }
+}
